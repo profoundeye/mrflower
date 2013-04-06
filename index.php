9f1bf58b5f17226246ba3426462df7789c9f326c
zplaying <profoundeye@gmail.com>
2013-04-06 21:58:02 +0800

    new add

---
+++ index.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>花瓶先生：您的私人花瓶管家，每周递送鲜花，让生活充满灵动的色彩</title>
+<meta content="按月订购,花瓶先生,鲜花" name=keywords>
+<meta content="按月订购鲜花，每周递送到您家" name="description">
+<link rel="stylesheet" type="text/css" href="a/s.css" />
+<script type="text/javascript" src="a/jquery.js"></script>
+</head>
+<body>
+	<div id="nav">
+		<div class="logo">花瓶先生
+		<p class="link"><span class="select" rel="0">本月</span><span rel="1">过往</span></p>
+		</ul>
+		</div>
+	</div>
+	<div class="mainFlower nowFlower">
+			<img src="https://hbloom-prod.s3.amazonaws.com/prod/prdb/images/26184/1362844766196-Parrot%20Tulips.jpg" />
+			<p class="dec">
+				<span>本周</span>
+				厄瓜多尔玫瑰称为玫瑰中的劳斯莱斯。厄瓜多尔庄园凯扬波镇，坐落在安第斯山海拔三千多米的高处、位于科托帕克希火山地区。roseonly此次情人节的精选玫瑰（True Love）全部采用厄瓜多尔庄园玫瑰，顶级“雪球”白色玫瑰花型硕大雪白与全部的爱搭配
+			</p>
+			<a href="#" class="order">立刻订购<span>，每月四次，仅需233元</span></a>
+	</div>
+	<div class="mainFlower list">
+		<ul>
+			<li>
+				<img src="https://hbloom-prod.s3.amazonaws.com/prod/prdb/images/26184/1362844766196-Parrot%20Tulips.jpg" />
+				<p class="dec">
+					<span>本周</span>
+					厄瓜多尔玫瑰称为玫瑰中的劳斯莱斯。厄瓜多尔庄园凯扬波镇，坐落在安第斯山海拔三千多米的高处、位于科托帕克希火山地区。roseonly此次情人节的精选玫瑰（True Love）全部采用厄瓜多尔庄园玫瑰，顶级“雪球”白色玫瑰花型硕大雪白与全部的爱搭配
+				</p>
+			</li>
+						<li>
+				<img src="https://hbloom-prod.s3.amazonaws.com/prod/prdb/images/26184/1362844766196-Parrot%20Tulips.jpg" />
+				<p class="dec">
+					<span>本周</span>
+					厄瓜多尔玫瑰称为玫瑰中的劳斯莱斯。厄瓜多尔庄园凯扬波镇，坐落在安第斯山海拔三千多米的高处、位于科托帕克希火山地区。roseonly此次情人节的精选玫瑰（True Love）全部采用厄瓜多尔庄园玫瑰，顶级“雪球”白色玫瑰花型硕大雪白与全部的爱搭配
+				</p>
+			</li>
+						<li>
+				<img src="https://hbloom-prod.s3.amazonaws.com/prod/prdb/images/26184/1362844766196-Parrot%20Tulips.jpg" />
+				<p class="dec">
+					<span>本周</span>
+					厄瓜多尔玫瑰称为玫瑰中的劳斯莱斯。厄瓜多尔庄园凯扬波镇，坐落在安第斯山海拔三千多米的高处、位于科托帕克希火山地区。roseonly此次情人节的精选玫瑰（True Love）全部采用厄瓜多尔庄园玫瑰，顶级“雪球”白色玫瑰花型硕大雪白与全部的爱搭配
+				</p>
+			</li>
+						<li>
+				<img src="https://hbloom-prod.s3.amazonaws.com/prod/prdb/images/26184/1362844766196-Parrot%20Tulips.jpg" />
+				<p class="dec">
+					<span>本周</span>
+					厄瓜多尔玫瑰称为玫瑰中的劳斯莱斯。厄瓜多尔庄园凯扬波镇，坐落在安第斯山海拔三千多米的高处、位于科托帕克希火山地区。roseonly此次情人节的精选玫瑰（True Love）全部采用厄瓜多尔庄园玫瑰，顶级“雪球”白色玫瑰花型硕大雪白与全部的爱搭配
+				</p>
+			</li>
+		</ul>
+	</div>
+	<div class="info">
+		<img class="wx" src="a/wx.gif" width="140px" height="140px" /><span class="wxh">微信号：花瓶先生</span>
+		<div class="logo">花瓶先生</div>
+		<span>提供每月定时鲜花送达服务，让生活充满灵动.</span>
+	</div>
+	<!--<div id="foot">
+		<ul>
+			<li>关于我们</li>
+		</ul>
+	</div>-->
+	<script type="text/javascript">
+		$(".link span").click(function(el){
+			$(".mainFlower").toggle();
+			})
+	</script>
+</body>
+</html>
